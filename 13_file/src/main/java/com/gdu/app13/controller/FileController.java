5c9c6e9bd17c258b89c09937c2cc7e8c752c99ab
gaeul56 <gaeul56@naver.com>
2023-10-15 21:17:42 +0900

    231015_2

--- 13_file/src/main/java/com/gdu/app13/controller/FileController.java
+++ 13_file/src/main/java/com/gdu/app13/controller/FileController.java
@@ -17,19 +17,21 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class FileController {
 
-  private final FileService fileService;
+  private final FileService fileService; //FileService를 주입받는 생성자 주입을 입한 필드
   
+  //파일 업로드를 처리하는 메소드
   @RequestMapping(value="/upload.do", method=RequestMethod.POST)
   public String upload(MultipartHttpServletRequest multipartRequest, RedirectAttributes redirectAttributes) {
-    int addResult = fileService.upload(multipartRequest);
-    redirectAttributes.addFlashAttribute("addResult", addResult);
-    return "redirect:/main.do";
+    int addResult = fileService.upload(multipartRequest); //업로드 서비스를 호출하여 파일 업로드를 수행
+    redirectAttributes.addFlashAttribute("addResult", addResult); //업로드 결과를 Flash 속성으로 추
+    return "redirect:/main.do"; //업로드 후에 메인 페이지로리디렉션
   }
   
+  //AJAX를 통해 파일 업로드를 처리하는 메소드
   @RequestMapping(value="/ajax/upload.do", method=RequestMethod.POST, produces="application/json")
   @ResponseBody
   public Map<String, Object> ajaxUpload(MultipartHttpServletRequest multipartRequest) {
-    return fileService.ajaxUpload(multipartRequest);
+    return fileService.ajaxUpload(multipartRequest); //AJAX 요청에 대한 업로드 서비스를 호출하고 JSON 응답을 반환
   }
   
 }
